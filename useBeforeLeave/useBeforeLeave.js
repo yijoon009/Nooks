c471a78fa9850e2d62ba430ab2173aa3f1bd3d23
yjkim <yijoon009@gmail.com>
2022-01-20 13:24:56 +0900

    useBeforeLeave

---
+++ useBeforeLeave/useBeforeLeave.js
@@ -0,0 +1,16 @@
+export const useBeforeLeave = (onBefore) => {
+  if (typeof onBefore !== "function") {
+    return;
+  }
+  const handle = (event) => {
+    const { clientY } = event;
+    //마우스가 위로 갔을때에만 onBefore() 실행
+    if (clientY <= 0) {
+      onBefore();
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
+    return () => document.removeEventListener("mouseleave", handle);
+  });
+};
