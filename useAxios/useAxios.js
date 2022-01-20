a37f4e83e665440355a4c73559a948ea842b997e
yjkim <yijoon009@gmail.com>
2022-01-20 16:51:05 +0900

    useAxios

---
+++ useAxios/useAxios.js
@@ -0,0 +1,34 @@
+export const useAxios = (opts, axiosInstance = defaultAxios) => {
+  const [state, setState] = useState({
+    loading: false,
+    error: null,
+    data: null,
+  });
+  const [trigger, setTrigger] = useState(0);
+  if (!opts.url) {
+    return;
+  }
+  const refetch = () => {
+    setState({
+      ...state,
+      loading: true,
+    });
+    //Date.now() 0이아닌 임의의 수를 리턴
+    setTrigger(Date.now());
+  };
+
+  useEffect(() => {
+    axiosInstance(opts)
+      .then((data) => {
+        setState({
+          ...state,
+          loading: false,
+          data,
+        });
+      })
+      .catch((error) => {
+        setState({ ...state, loading: false, error });
+      });
+  }, [trigger]);
+  return { ...state, refetch };
+};
