da76cfaab6b0436817eeff5097cff41c497a9383
yjkim <yijoon009@gmail.com>
2022-01-20 09:51:44 +0900

    useClick

---
+++ useClick/useClick.js
@@ -0,0 +1,17 @@
+export const useClick = (onClick) => {
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  if (typeof onClick !== "function") {
+    return;
+  }
+  return element;
+};
